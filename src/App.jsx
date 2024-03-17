3c10a34dc65787df23055d93efeaf5e4a74a2a15
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-03-17 17:24:29 +0530

    display fixed

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg text-center px-4  py-3 my-8 text-orange-500 bg-gray-700 ">
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg text-center px-4  py-3 my-8 text-orange-500 bg-gray-700 fixed bottom-48 left-0 right-0">
         <h1 className="text-white text-center my-3">PasswordGernator</h1>
         <div className=" flex shadow rounded-lg overflow-hidden mb-4">
           <input
